ce7db71d047c1f550f092ed0eebe93cceadbdac8
Dennis Duka <GreyTeddy@users.noreply.github.com>
2020-02-17 15:33:49 +0000

    remove bottom border from description

--- comments_remove.js
+++ comments_remove.js
@@ -1,5 +1,10 @@
-window.onload = function(){
+window.onload = function () {
     //gets the "comments" <ytd-comments> div amd removes it
-    var comments = document.getElementById("comments");
+    let comments = document.getElementById("comments");
     comments.parentNode.removeChild(comments);
+
+    //removes the the bottom border (on the secondary info)
+    let description = document.getElementsByTagName("ytd-video-secondary-info-renderer")[0];
+    if (description) description.style.border = "none";
+
 }
